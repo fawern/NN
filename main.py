40234d5a2b6fe30f1b37e1c07cc41be1ef70606e
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-03-06 13:10:41 +0300

    LayersWrap added

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd 
 import numpy as np 
-np.random.seed(0)
+# np.random.seed(0)
 
 from sklearn.model_selection import train_test_split
 
 from Nn import Layers
-from Nn import NLayer
+
 from Nn import NInput
+from Nn import NLayer
 
 inputs = []
 outputs = []
@@ -22,6 +23,7 @@ class DataGenerator:
 
     def generate_data(self):
         for i in range(self.num_row):
+
             height = np.random.uniform(0.5, 2.5)
             weight = np.random.randint(10, 200)
 
@@ -36,44 +38,74 @@ class DataGenerator:
             output = np.random.randint(0, 2)
 
             self.df.loc[len(self.df)] = [height, weight, eye_color, hair_color, output]
-            
+
         return self.df
 
-data_generator = DataGenerator(5)
-# data_generator.add_feature([1, 2, 3, 4, 5, 6, 7])
+    @staticmethod
+    def train_test_split(X, y, train_rate):
+        num_train = int(len(X) * train_rate)
+        indices = np.random.permutation(len(X))
+        train_indices, test_indices = indices[:num_train], indices[num_train:]
+        X_train, X_test = X[train_indices], X[test_indices]
+        y_train, y_test = y[train_indices], y[test_indices]
+        return X_train, X_test, y_train, y_test
 
+data_generator = DataGenerator(60)
+# data_generator.add_feature([1, 2, 3, 4, 5, 6, 7])
 df = data_generator.generate_data()
 
 X = df.drop(columns='gender').values
 y = df['gender'].values
- 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-model = Layers()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8)
 
-# model.add(NInput(num_neurons=X_train.shape[1]))
+class LayersWrap(Layers):
+    '''
+    LayersWrap class is a wrapper class for the Layers class.
+    '''
+    def __init__(self, hl_activation, ol_activation):
 
-## Input Layer 
-model.add(NLayer(num_neurons=X_train.shape[1], use_bias=True)) # egitild
+        """
+        Args:
+            - hl_activation (str): The activation function of hidden layers.
+            - ol_activation (str): The activatoin function of output layer
+        """
 
-## Hidden Layers
-model.add(NLayer(num_neurons=256, activation='softmax', use_bias=True)) 
-model.add(NLayer(num_neurons=128, activation='softmax', use_bias=True)) 
-model.add(NLayer(num_neurons=64, activation='sigmoid', use_bias=True)) 
+        ## Initialize the layers class.
+        Layers.__init__(self)
 
-## Output Layer
-model.add(NLayer(num_neurons=1, activation='sigmoid', use_bias=False))
+        self.hl_activation = hl_activation
+        self.ol_activation = ol_activation
 
-## Train model
-model.train_model(x=X_train)
+        ## Input Layer
+        self.add(NInput(num_neurons=X_train.shape[1]))
 
-y_pred = model.output
+        ## Hidden Layers
+        self.add(NLayer(num_neurons=256, activation=self.hl_activation, use_bias=True)) 
+        self.add(NLayer(num_neurons=128, activation=self.hl_activation, use_bias=True)) 
+        self.add(NLayer(num_neurons=64, activation=self.hl_activation, use_bias=True)) 
 
-print(y_pred)
+        ## Output Layer
+        self.add(NLayer(num_neurons=1, activation=self.ol_activation, use_bias=False))
+
+        ## Train model
+        self.train_model(x=X_train, y=y_train, iterations=2)
+
+    def get_accuracy(self):
+        return self.output
+
+model = LayersWrap(hl_activation='softmax', ol_activation='sigmoid')
+
+y_output = model.get_accuracy()
+
+print('\n')
+print(f'Accuracy of model : {model.evaluate_trained_model()}')
 
 def gender(output):
     return 'Female' if output > 0.5 else "Male"
 
 print('\n')
-for i, output in enumerate(y_pred):
-    print(f'Prediction for input {i+1}: {gender(output)}, true value is {"Kadin" if y_pred[i] == 1 else "Erkek"}')
+for i, output in enumerate(y_output):
+    predicted_gender = gender(output)
+    true_gender = "Female" if y_train[i] == 1 else "Male"
+    print(f'Prediction for input {i+1}: {predicted_gender}, true value is {true_gender}')
