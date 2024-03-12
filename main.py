e1d7255a3263fa8bb5bb53f5c99ee93a1df79f6b
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-03-12 09:41:43 +0300

    loss plot added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 import numpy as np 
-# np.random.seed(0)
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
 
 from sklearn.model_selection import train_test_split
 
@@ -50,21 +52,21 @@ class DataGenerator:
         y_train, y_test = y[train_indices], y[test_indices]
         return X_train, X_test, y_train, y_test
 
-data_generator = DataGenerator(1000)
-# data_generator.add_feature([1, 2, 3, 4, 5, 6, 7])
-df = data_generator.generate_data()
+# data_generator = DataGenerator(1000)
+# # data_generator.add_feature([1, 2, 3, 4, 5, 6, 7])
+# df = data_generator.generate_data()
 
-X = df.drop(columns='gender').values
-y = df['gender'].values
+# X = df.drop(columns='gender').values
+# y = df['gender'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8)
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [1, 1], [1, 1], [0, 0], [0, 1], [1, 0], [1, 1], [1, 1], [1, 1]])
+y = np.array([[0], [1], [1], [0], [0], [0], [0], [1], [1], [0], [0], [0]])
 
 class LayersWrap(Layers):
     '''
     LayersWrap class is a wrapper class for the Layers class.
     '''
-    def __init__(self, hl_activation, ol_activation):
-
+    def __init__(self, input_data, output_data, hl_activation, ol_activation):
         """
         Args:
             - hl_activation (str): The activation function of hidden layers.
@@ -74,28 +76,38 @@ class LayersWrap(Layers):
         ## Initialize the layers class.
         Layers.__init__(self)
 
+        self.input_data = input_data
+        self.output_data = output_data
         self.hl_activation = hl_activation
         self.ol_activation = ol_activation
 
         ## Input Layer
-        self.add(NInput(num_neurons=X_train.shape[1]))
+        self.add(NInput(num_neurons=self.input_data.shape[1]))
 
         ## Hidden Layers
-        self.add(NLayer(num_neurons=256, activation=self.hl_activation, use_bias=True)) 
         self.add(NLayer(num_neurons=128, activation=self.hl_activation, use_bias=True)) 
         self.add(NLayer(num_neurons=64, activation=self.hl_activation, use_bias=True)) 
+        self.add(NLayer(num_neurons=4, activation=self.hl_activation, use_bias=True)) 
 
         ## Output Layer
-        self.add(NLayer(num_neurons=1, activation=self.ol_activation, use_bias=False))
+        self.add(NLayer(num_neurons=output_data.shape[1], activation=self.ol_activation, use_bias=False))
 
         ## Train model
-        self.train_model(x=X_train, y=y_train, iterations=6)
+    
+    def fit_model(self, learning_rate=0.1, iterations=1000000):
+        self.train_model(x=self.input_data, y=self.output_data, iterations=iterations)
 
     def get_accuracy(self):
         return self.output
 
-model = LayersWrap(hl_activation='softmax', ol_activation='sigmoid')
+model = LayersWrap(
+    input_data=X, output_data=y, 
+    hl_activation='softmax', ol_activation='sigmoid', 
+)
+model.fit_model(learning_rate=0.001, iterations=1000)
 
-y_output = model.get_accuracy()
+losses = model.losses
 
-print(f'Accuracy of model : {model.evaluate_trained_model()}')
+plt.figure(figsize=(5, 3))
+plt.title('Losses')
+plt.plot(losses)
