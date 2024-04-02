9b06948b6eacdb1d2edda4cddcb95b100d96f5c2
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-04-02 11:00:13 +0300

    model trained on real data.

--- mlp_classification.py
+++ mlp_classification.py
@@ -11,11 +11,10 @@ from Nn import Layers
 from Nn import NInput
 from Nn import NLayer
 
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [1, 1], [1, 1], [0, 0], [0, 1], [1, 0], [1, 1], [1, 1], [1, 1]])
-y = np.array([[0], [1], [1], [0], [0], [0], [0], [1], [1], [0], [0], [0]])
-
 df = pd.read_csv("./gender_classification_v7.csv")
-print(df.tail())
+np.random.shuffle(df.values)
+df = df.iloc[:100]
+
 # mapping gender to 0 and 1
 df['gender'] = df['gender'].map({"Male" : 0 , "Female" : 1})
 
@@ -63,7 +62,7 @@ model = LayersWrap(
     hl_activation='sigmoid', ol_activation='sigmoid', 
 )
 
-model.fit_model(learning_rate=0.01, iterations=100, batch_size=4)
+model.fit_model(learning_rate=0.0001, iterations=100000, batch_size=1)
 
 print(model.evaluate_trained_model())
 
